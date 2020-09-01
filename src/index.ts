b2a2923e7245af739122874951b46018d8668077
Daniel Grefberg <hello@danielgrefberg.com>
2020-09-01 20:08:48 +0200

    fix: improve performance and reduce package size

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,72 @@
-import cropImageData, { ImageDataLike } from 'crop-image-data';
+import cropImageData, { CropOptions, ImageDataLike } from 'crop-image-data';
 
-interface RGBA {
+type ImageDataLikeData = Uint8ClampedArray | number[];
+
+export interface RGBA {
   red: number;
   green: number;
   blue: number;
   alpha: number;
 }
 
-interface RGBAOptions {
-  data: number[] | Uint8ClampedArray;
-  width: number;
-  x: number;
-  y: number;
-}
+export type TrimColorFunc = (rgba: RGBA) => boolean;
 
-type TrimColorFunc = (rgba: RGBA) => boolean;
+type Side = 'top' | 'right' | 'bottom' | 'left';
 
-interface ScanOptions {
+export interface TrimOptions {
   trimColor: TrimColorFunc;
-  reverse: boolean;
-}
-
-interface TrimOptions {
-  trimColor: TrimColorFunc;
-}
-
-function getRGBA({ data, width, x, y }: RGBAOptions): RGBA {
-  const index = (width * y + x) * 4;
-  return {
-    red: data[index],
-    green: data[index + 1],
-    blue: data[index + 2],
-    alpha: data[index + 3],
-  };
-}
-
-function scanY(imageData: ImageDataLike, { trimColor, reverse }: ScanOptions): number | null {
-  const { data, width, height } = imageData;
-  const offset = reverse ? -1 : 1;
-  const firstCol = reverse ? height - 1 : 0;
-
-  // loop through each row
-  for (let y = firstCol; reverse ? y > -1 : y < height; y += offset) {
-    // loop through each column
-    for (let x = 0; x < width; x++) {
-      const rgba = getRGBA({ data, width, x, y });
-      if (trimColor(rgba)) {
-        // return number of rows from edge
-        return reverse ? firstCol - y : y;
-      }
-    }
-  }
-
-  // the whole image should be trimmed
-  return null;
-}
-
-// finds the first x coord in imgData that is not white
-function scanX(imageData: ImageDataLike, { trimColor, reverse }: ScanOptions): number | null {
-  const { data, width, height } = imageData;
-  const offset = reverse ? -1 : 1;
-  const firstRow = reverse ? width - 1 : 0;
-
-  // loop through each column
-  for (let x = firstRow; reverse ? x > -1 : x < width; x += offset) {
-    // loop through each row
-    for (let y = 0; y < height; y++) {
-      const rgba = getRGBA({ data, width, x, y });
-      if (trimColor(rgba)) {
-        // return number of columns from edge
-        return reverse ? firstRow - x : x;
-      }
-    }
-  }
-
-  // the whole image should be trimmed
-  return null;
 }
 
 function getEmptyImageData() {
   return new ImageData(1, 1);
 }
 
+function getRGBA(data: ImageDataLikeData, i: number): RGBA {
+  return { red: data[i], green: data[i + 1], blue: data[i + 2], alpha: data[i + 3] };
+}
+
+function scanSide(imageData: ImageDataLike, side: Side, trimColor: TrimColorFunc) {
+  const { data, width, height } = imageData;
+  const horizontal = side === 'left' || side === 'right';
+  const reverse = side === 'right' || side === 'bottom';
+  const primaryAxis = horizontal ? width : height;
+  const secondaryAxis = horizontal ? height : width;
+  const step = reverse ? -1 : 1;
+  const start = reverse ? primaryAxis - 1 : 0;
+
+  // loop through each column
+  for (let p = start; reverse ? p > -1 : p < primaryAxis; p += step) {
+    // loop through each row
+    for (let s = 0; s < secondaryAxis; s++) {
+      const index = (horizontal ? width * s + p : width * p + s) * 4;
+      const rgba = getRGBA(data, index);
+      if (trimColor(rgba)) {
+        // return number of columns from edge
+        return reverse ? start - p : p;
+      }
+    }
+  }
+
+  // the whole image should be trimmed
+  return null;
+}
+
 export default function trimImageData(imageData: ImageDataLike, trimOptions?: TrimOptions) {
   const trimColor: TrimColorFunc = trimOptions?.trimColor || (({ alpha }) => !!alpha);
 
-  const top = scanY(imageData, { reverse: false, trimColor });
-  if (top === null) {
-    return getEmptyImageData();
+  const cropOptions: CropOptions = {};
+  const sides: Side[] = ['top', 'bottom', 'left', 'right'];
+
+  for (let i = 0; i < sides.length; i++) {
+    const side = sides[i];
+    const crop = scanSide(imageData, side, trimColor);
+
+    if (crop === null) {
+      return getEmptyImageData();
+    }
+
+    cropOptions[side] = crop;
   }
 
-  const right = scanX(imageData, { reverse: true, trimColor });
-  if (right === null) {
-    return getEmptyImageData();
-  }
-
-  const bottom = scanY(imageData, { reverse: false, trimColor });
-  if (bottom === null) {
-    return getEmptyImageData();
-  }
-
-  const left = scanX(imageData, { reverse: true, trimColor });
-  if (left === null) {
-    return getEmptyImageData();
-  }
-
-  return cropImageData(imageData, { top, right, bottom, left });
+  return cropImageData(imageData, cropOptions);
 }
