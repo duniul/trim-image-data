3b0cdc94fad6289b3fc296385f3b925755fa2cf3
Daniel Grefberg <hello@danielgrefberg.com>
2020-09-03 17:36:45 +0200

    chore: add changelog and git plugins for semantic-release

--- release.config.js
+++ release.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
